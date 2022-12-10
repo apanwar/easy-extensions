fe40ac2574dcb82aea8a113de33954d5ca2ac316
Amit Panwar <panwar.amit@hotmail.com>
2022-12-10 13:30:09 +0000

    added model converter registration

--- easytypedemo/src/main/groovy/Init.groovy
+++ easytypedemo/src/main/groovy/Init.groovy
@@ -3,6 +3,14 @@ import org.slf4j.LoggerFactory
 
 def LOG = LoggerFactory.getLogger("easytypedemo")
 
+def defaultConverterRegistry = spring.getBean("defaultConverterRegistry")
+
+LOG.info("Registering mode converted for EasyItem")
+
+defaultConverterRegistry.registerModelConverter("EasyItem", EasyItemModel.class, null)
+
+LOG.info("Registered model converter for EasyITem")
+
 def easyTypesRegistry = spring.getBean("easyTypesRegistry")
 
 easyTypesRegistry.registerClass("com.sap.cx.boosters.easy.extension.easytypedemo.model.EasyItemModel", EasyItemModel.class)
