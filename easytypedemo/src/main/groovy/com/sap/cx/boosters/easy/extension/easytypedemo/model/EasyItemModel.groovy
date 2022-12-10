7dc810de79fb39e5d46b684a9efcdb6fd959e51c
Amit Panwar <panwar.amit@hotmail.com>
2022-12-10 10:08:12 +0000

    added easy type demo

---
+++ easytypedemo/src/main/groovy/com/sap/cx/boosters/easy/extension/easytypedemo/model/EasyItemModel.groovy
@@ -0,0 +1,47 @@
+package com.sap.cx.boosters.easy.extension.easytypedemo.model
+
+import de.hybris.bootstrap.annotations.Accessor
+import de.hybris.platform.core.model.ItemModel
+
+class EasyItemModel extends ItemModel {
+    public static final String _TYPECODE = "EasyItem"
+
+    public static final String CODE = "code";
+	
+	public static final String NAME = "name";
+
+    public EasyItemModel()
+	{
+		super();
+	}
+	
+	public EasyItemModel(final ItemModelContext ctx)
+	{
+		super(ctx);
+	}
+
+    @Accessor(qualifier = "code", type = Accessor.Type.GETTER)
+	public String getCode()
+	{
+		return getPersistenceContext().getPropertyValue(CODE);
+	}
+
+    @Accessor(qualifier = "code", type = Accessor.Type.SETTER)
+	public void setCode(final String value)
+	{
+		getPersistenceContext().setPropertyValue(CODE, value);
+	}
+
+    @Accessor(qualifier = "name", type = Accessor.Type.GETTER)
+	public String getName()
+	{
+		return getName(null);
+	}
+
+	@Accessor(qualifier = "name", type = Accessor.Type.GETTER)
+	public String getName(final Locale loc)
+	{
+		return getPersistenceContext().getLocalizedValue(NAME, loc);
+	}
+	
+}
