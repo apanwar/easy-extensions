0eb006b4a447f101e0d7883a2b349f06ada88b55
Amit Panwar <panwar.amit@hotmail.com>
2022-12-10 13:26:24 +0000

    registered model to easy types registry

--- easytypedemo/src/main/groovy/Init.groovy
+++ easytypedemo/src/main/groovy/Init.groovy
@@ -3,5 +3,8 @@ import org.slf4j.LoggerFactory
 
 def LOG = LoggerFactory.getLogger("easytypedemo")
 
+def easyTypesRegistry = spring.getBean("easyTypesRegistry")
+
+easyTypesRegistry.registerClass("com.sap.cx.boosters.easy.extension.easytypedemo.model.EasyItemModel", EasyItemModel.class)
 
 LOG.info("Init completed for easytype demo")
